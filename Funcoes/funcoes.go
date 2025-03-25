39a5558fac563e90ea596651a46b7f03a16d1a20
mauricioandrade <mauricio_andrade@outlook.com>
2025-03-24 22:02:03 -0300

    funcoes golang

---
+++ Funcoes/funcoes.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	somaDosValores := soma(42, 13)
+	fmt.Println(somaDosValores)
+	subtrairValores := subtrair(42, 13)
+	fmt.Println(subtrairValores)
+	idadeCalculo := idade(2025, 1992)
+	fmt.Println(idadeCalculo)
+	multiplicarValores := multiplicar(30, 30)
+	fmt.Println(multiplicarValores)
+	decimaisValores := decimais(12.3, 14.6)
+	fmt.Println(decimaisValores)
+
+	nome1, sobrenome := printaNome("Mauricio", "Andrade")
+	fmt.Println(nome1)
+	fmt.Println(sobrenome)
+
+	
+	fmt.Println(printaDiasUteis())
+
+
+}
+
+//função começando com letra minuscula:
+// FUNÇÃO É PRIVADA 
+// ELA SÓ PODE SER UTILIZADA NO PACOTE MAIN ex printaNome
+
+//função começando com letra maiscula:
+// FUNÇÃO É PUBLICA
+// PODE SER CHAMADA EM OUTRO PACOTE ex PrintaNome
+// como utilizar ela fora do pacote, ex main.PrintaNome(nome)
+
+func printaNome(nome, sobrenome string) (string, string) {
+	return nome, sobrenome
+}
+
+func printaDiasUteis() string {
+	diasUteis := "Segunda, Terça, Quarta, Quinta, Sexta"
+	return diasUteis
+}
+
+func soma(x int, y int) int {
+	return x + y
+}
+
+func multiplicar(x int, y int) int {
+	return x * y
+}
+
+func decimais(x float64, y float64) float64 {
+	return x + y
+}
+
+func subtrair(x int, y int) int {
+	return x - y
+}
+
+func idade(anoatual int, anonascimento int) int {
+	return anoatual - anonascimento
+}
