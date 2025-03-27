314564bfdaeae3bf803c6e6a2f534d4575a69da0
mauricioandrade <mauricio_andrade@outlook.com>
2025-03-27 20:06:55 -0300

    loops com for em golang

--- Loops/loops.go
+++ Loops/loops.go
@@ -12,16 +12,26 @@ func main() {
 	// i-- -> subtraindo 1
 
 	// for range
-	frutas := []string{"Laranja", "Maça", "Banana", "Uva", "Kiwi"}
-	for i, fruta := range frutas {
-		fmt.Println("Fruta",fruta, "indice", i)
+	// frutas := []string{"Laranja", "Maça", "Banana", "Uva", "Kiwi"}
+	// for i, fruta := range frutas {
+	// 	fmt.Println("Fruta",fruta, "indice", i)
+	// }
+
+	champs := []string{"Kai'sa", "Caitlyn", "Vayne", "Lucian", "Ezreal"}
+	for _, champ := range champs {
+		fmt.Println("Os Champs são :", champ)
 	}
 
-	sum := 0
-	for i := 0; i <= 10; i++ {
-		fmt.Println("Sum: ", sum)
-		fmt.Println("Indice: ", i)
-		sum += i
+	// sum := 0
+	// for i := 0; i <= 10; i++ {
+	// 	fmt.Println("Sum: ", sum)
+	// 	fmt.Println("Indice: ", i)
+	// 	sum += i
+
+	// contando até 10
+		for i := 0; i <=10; i++{
+			fmt.Println(i)
+		}
 
 		// é a mesma coisa que: sum = sum + i
 		// sum -= i -> sum = sum - i
@@ -29,6 +39,6 @@ func main() {
 		// é como se ao final do loop fosse adcionado 1 ao indice i
 		// i++
 		// i = i + 1
+
 	}
-	fmt.Println(sum)
-}
+
